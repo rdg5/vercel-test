f116478c59b680b76e7252a9d9c0a2871a435c98
sanyi <sandor.vass@tuta.io>
2025-02-13 10:49:27 +0100

    feat(): add vercel test

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,3 @@
+export const getTimestamp = () => {
+  return `Timestamp: ${new Date().toISOString()}`;
+};
---
+++ packages/web/src/index.ts
@@ -0,0 +1,4 @@
+import { getTimestamp } from "@repro/shared";
+
+console.log("Web package initialized");
+console.log(getTimestamp());
