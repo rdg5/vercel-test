3a151fe61795a06cd08075ba9d77d1c50c723f83
sanyi <sandor.vass@tuta.io>
2025-02-13 13:55:02 +0100

    as

---
+++ apps/member-ui/nuxt.config.ts
@@ -0,0 +1,7 @@
+export default defineNuxtConfig({
+  srcDir: "src",
+  modules: ["@repro/asf3"],
+  nitro: {
+    preset: "bun",
+  },
+});
---
+++ apps/member-ui/src/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Member UI</h1>
+    <p>Using @repro/asf3 module</p>
+  </div>
+</template>
